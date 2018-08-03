4df3d0231558f77a5ad17ac1bcb9fdf7e28ff7c2
TCWhite <tingchun.pai@hotmail.com>
2018-08-03 20:18:26 +0800

    Add categorization tag

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -2,45 +2,33 @@
 
 // Create the array of quote objects and name it quotes
 var stringOfQuoteProperties = '';
-var quotes =[
+var quotes = [
   {
     quote: 'Have no fear of perfection, you will never reach it.',
     source: 'Salvador Dali',
-    citation: 'Germany',
-    year: '1950'
+    categorization: 'Art'
   },
   {
     quote: 'You never fail until you stop trying.',
     source: 'Albert Einstein',
-    citation: '',
-    year: ''
+    categorization: 'Science'
   },
   {
     quote: 'If you want to run, run a mile. If you want to experience a different life, run a marathon.',
     source: 'Emil Zatopek',
-    citation: '',
-    year: ''
+    categorization: 'Marathon'
   },
   {
     quote: 'The pride in finishing a marathon is much greater than all the pain endured during the marathon.',
     source: 'Hal Higdon',
-    citation: '',
-    year: ''
+    categorization: 'Marathon'
   },
   {
     quote: 'The miracle is not that I finished. It is that I had the courage to start.',
     source: 'John Bingham',
-    citation: '',
-    year: ''
+    categorization: 'Marathon'
   }
 ]
-
-function getStudentReport (student) {
-  var report = '<h2>Student: ' + student.name + '</h2>';
-  report += '<p>Track: ' + student.track + '</p>';
-  report += '<p>Points: ' + student.points + '</p>';
-  report += '<p>Achievements: ' + student.achievements + '</p>';
-}
 
 // Create the getRandomQuote function and name it getRandomQuote
 
@@ -49,15 +37,15 @@ var randomNumber = Math.floor(Math.random() * quotes.length );
 displayQuote = quotes [randomNumber];
 stringOfQuoteProperties = '<p class="quote">' + displayQuote.quote + '</p>';
 stringOfQuoteProperties += '<p class="source">' + displayQuote.source + '</p>';
-stringOfQuoteProperties += '<span class="citation">' + displayQuote.citation + '</span>';
-stringOfQuoteProperties += '<span class="year">' + displayQuote.year + '</span>';
+stringOfQuoteProperties += '<h1>' + displayQuote.categorization + '</h1>'
 }
 
 // Create the printQuote funtion and name it printQuote
 
 function printQuote () {
   getRandomQuote();
-document.getElementById('quote-box').innerHTML = stringOfQuoteProperties;
+  document.getElementById('quote-box').innerHTML = stringOfQuoteProperties;
+  myFunction();
 }
 
 // This event listener will respond to "Show another quote" button clicks
